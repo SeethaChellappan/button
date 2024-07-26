453d15ed33cc1cf7caea11bc770c68aacdf2cbc7
SeethaChellappan <seetha.chellappan@gmail.com>
2024-07-26 12:21:53 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.scss';
 
 const App = () =>
 	<div className="App">
-		Ready to start.
-	</div>;
+		<a href="https://www.freecodecamp.org/">
+			<button style={ { color: 'red' } }>Button1</button>
+		</a>
+		<a href="https://www.w3schools.com/">
+			<button style={ { color: 'green' } }>Button2</button>
+		</a>
+	</div>
+	;
 
 export default App;
